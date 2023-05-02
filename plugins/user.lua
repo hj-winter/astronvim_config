a17d91985c94e76148d62f391f032a32cd537354
Hermann-Josef Winter <hjw2803@googlemail.com>
2023-05-02 22:43:36 +0200

    add nvim-surround

--- plugins/user.lua
+++ plugins/user.lua
@@ -24,4 +24,14 @@ return {
       { "<leader>B", "<cmd>Telescope buffers<cr>", desc = "Open buffers in Telescope" },
     },
   },
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup {
+        -- Configuration here, or leave empty to use defaults
+      }
+    end,
+  },
 }

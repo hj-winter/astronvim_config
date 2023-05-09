a17c962517201ddd95488061c128e04012d3cc36
Hermann-Josef Winter <hjw2803@googlemail.com>
2023-05-09 20:45:29 +0200

    add barbecue.nvim

--- plugins/user.lua
+++ plugins/user.lua
@@ -34,4 +34,16 @@ return {
       }
     end,
   },
+  {
+    "utilyre/barbecue.nvim",
+    name = "barbecue",
+    version = "*",
+    dependencies = {
+      "SmiteshP/nvim-navic",
+      "nvim-tree/nvim-web-devicons", -- optional dependency
+    },
+    opts = {
+      -- configurations go here
+    },
+  },
 }
